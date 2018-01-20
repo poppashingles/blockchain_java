2fc22507f58c8eccb3d898f87cfb74a3efb89df1
Rob <robdwilliams85@gmail.com>
2018-01-20 19:51:21 +0000

    Created the transaction class with defined variables

---
+++ src/Transaction.java
@@ -0,0 +1,12 @@
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class Transaction {
+    public String transactionId;
+    public PublicKey sender;
+    public PublicKey recipient;
+    public float value;
+    public byte[] signature;
+
+
+}
